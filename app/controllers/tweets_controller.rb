dad1bfd5f1226532f2b27b38748c630f1d9e7f7b
zachary mines <zmines@wesleyan.edu>
2020-09-22 21:11:46 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,8 @@
 class TweetsController < ApplicationController
   
-  get '/tweets:id' do 
+  get '/tweets:id' do
     if logged_in?
+      @tweets = Tweet.all
       erb :'tweets/tweets'
     else 
       erb :'users/login'
