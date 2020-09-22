f353ab1f19a95fc7f226eb0d666d336cbb9cd40c
zachary mines <zmines@wesleyan.edu>
2020-09-22 18:39:26 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
 
-  get '/' do 
-    erb :layout
+  get '/users' do 
+    erb :index
   end 
   
-  
+  post '/start' do 
+    # binding.pry 
+    if params[:choice] == "Login"
+      erb :login 
+    elsif params[:choice] == "Sign Up"
+      erb :create_user 
+    end 
+  end 
 
+  post '/login' do 
+  end 
+  
+  post '/signup' do 
+  end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_09_21_173753) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
+    t.string "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
