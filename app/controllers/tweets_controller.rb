fdb4882f8c59b9e44ef7b5bc6b482003549cbc71
zachary mines <zmines@wesleyan.edu>
2020-09-25 20:47:42 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,8 +12,13 @@ class TweetsController < ApplicationController
     end 
   end 
   
-  get '/tweets/:id/edit' do 
+  get '/tweets/:id/edit/' do 
+    # binding.pry
+    @tweet = Tweet.find_by_id(params[:id])
     erb :'tweets/edit_tweet'
+  end 
+  
+  post '/tweets/:id/edit/' do 
   end 
     
   
@@ -22,7 +27,7 @@ class TweetsController < ApplicationController
   end 
   
   get '/delete/:id' do
-    binding.pry
+    # binding.pry
     @tweet = Tweet.find_by_id(params[:id])
     erb :"tweets/delete"
   end 
@@ -37,10 +42,14 @@ class TweetsController < ApplicationController
   end
   
   delete '/tweets/:id' do 
-    binding.pry
-    @tweet = Tweet.find_by_id(params[:id])
-    @tweet.delete 
-    redirect to '/deleted'
+    # binding.pry
+    if params[:choice] == "No, take me back"
+      redirect to '/tweets/:id'
+    else 
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.delete 
+      redirect to '/deleted'
+    end 
   end 
   
 
