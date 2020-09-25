c7ff65b427762675f6a14b8ac739a3c27f9f9bd9
zachary mines <zmines@wesleyan.edu>
2020-09-25 22:40:45 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,11 @@ class UsersController < ApplicationController
   end 
 
   get '/login' do 
-    erb :'users/login'
+    if logged_in?
+      redirect to '/tweets'
+    else 
+      erb :'users/login'
+    end 
   end 
   
   get '/signup' do
